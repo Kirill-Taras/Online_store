2ce53a031e1cc62461bb044026451d3d4ea7303d
kirill_tarasov <kirilltaras1792@yandex.ru>
2024-03-03 19:41:21 +0700

    Исправить настройки url

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from catalog.views import home
 from catalog.apps import CatalogConfig
 
-catalog_name = CatalogConfig.name
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", home, name="catalog"),
